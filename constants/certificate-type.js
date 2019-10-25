462d6afce5f55da2fc22eef626d3549fe27dc2db
Kelp <kelp@phate.org>
2019-10-25 15:28:23 +0800

    Added https certificate schema.

---
+++ constants/certificate-type.js
@@ -0,0 +1,8 @@
+module.exports = {
+  selfSigned: 0,
+  uploadCertificate: 1,
+  generateCertificate: 2,
+  all() {
+    return [this.selfSigned, this.uploadCertificate, this.generateCertificate];
+  }
+};
