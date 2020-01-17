eecbf724b6dad31120ffa3dfbdc0501e83da4800
Kelp <kelp@androvideo.com>
2020-01-17 16:20:32 +0800

    feat: add the notification app settings schema

---
+++ notification-app-settings-schema.js
@@ -0,0 +1,48 @@
+module.exports = {
+  deviceToken: {
+    // 裝置金鑰
+    optional: false,
+    type: 'string',
+    empty: false,
+    max: 1024
+  },
+  deviceId: {
+    // 裝置編碼
+    optional: false,
+    type: 'string',
+    empty: false,
+    max: 1024
+  },
+  interval: {
+    // 通知時間間隔
+    optional: false,
+    type: 'custom',
+    pattern: /^[\d]{1,4}$/,
+    min: 5,
+    max: 1800,
+    check: function (value, schema) {
+      if (schema.optional && (value == null || value === '')) {
+        return true;
+      }
+
+      if (typeof value !== 'string') {
+        return this.makeError('string', null, value);
+      }
+
+      if (!schema.pattern.test(value)) {
+        return this.makeError('stringPattern', schema.pattern, value);
+      }
+
+      const frameRate = Number(value);
+      if (frameRate < schema.min) {
+        return this.makeError('numberMin', schema.min, value);
+      }
+
+      if (frameRate > schema.max) {
+        return this.makeError('numberMax', schema.max, value);
+      }
+
+      return true;
+    }
+  }
+};
