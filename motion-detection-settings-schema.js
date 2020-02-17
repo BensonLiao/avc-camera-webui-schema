3c0c89d811571835df55d28ceaafda1f284f3788
Kelp <kelp@androvideo.com>
2020-02-17 16:57:22 +0800

    feat: add motion detection settings schema

---
+++ motion-detection-settings-schema.js
@@ -0,0 +1,48 @@
+module.exports = {
+  isEnable: {
+    // 辨識功能
+    type: 'boolean'
+  },
+  sensibility: {
+    // 靈敏度
+    optional: false,
+    type: 'number',
+    min: 1,
+    max: 5,
+    integer: true
+  },
+  areas: {
+    // 觸發區域
+    type: 'object',
+    props: {
+      x: {
+        optional: false,
+        type: 'number',
+        min: 0,
+        max: 100,
+        integer: true
+      },
+      y: {
+        optional: false,
+        type: 'number',
+        min: 0,
+        max: 100,
+        integer: true
+      },
+      width: {
+        optional: false,
+        type: 'number',
+        min: 0,
+        max: 100,
+        integer: true
+      },
+      height: {
+        optional: false,
+        type: 'number',
+        min: 0,
+        max: 100,
+        integer: true
+      }
+    }
+  }
+};
