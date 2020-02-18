e3d7861d48188a5525a0fc967cfeb04689c3de5d
Kelp <kelp@androvideo.com>
2020-02-18 11:35:46 +0800

    fix: fix the data type of `areas` of motion detection settings schema

--- motion-detection-settings-schema.js
+++ motion-detection-settings-schema.js
@@ -13,35 +13,42 @@ module.exports = {
   },
   areas: {
     // 觸發區域
-    type: 'object',
-    props: {
-      x: {
-        optional: false,
-        type: 'number',
-        min: 0,
-        max: 100,
-        integer: true
-      },
-      y: {
-        optional: false,
-        type: 'number',
-        min: 0,
-        max: 100,
-        integer: true
-      },
-      width: {
-        optional: false,
-        type: 'number',
-        min: 0,
-        max: 100,
-        integer: true
-      },
-      height: {
-        optional: false,
-        type: 'number',
-        min: 0,
-        max: 100,
-        integer: true
+    optional: false,
+    type: 'array',
+    empty: true,
+    min: 0,
+    max: 4,
+    items: {
+      type: 'object',
+      props: {
+        x: {
+          optional: false,
+          type: 'number',
+          min: 0,
+          max: 100,
+          integer: true
+        },
+        y: {
+          optional: false,
+          type: 'number',
+          min: 0,
+          max: 100,
+          integer: true
+        },
+        width: {
+          optional: false,
+          type: 'number',
+          min: 0,
+          max: 100,
+          integer: true
+        },
+        height: {
+          optional: false,
+          type: 'number',
+          min: 0,
+          max: 100,
+          integer: true
+        }
       }
     }
   }
