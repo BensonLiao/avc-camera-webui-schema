028bd36a142a112c975c480678ba8e65bdf742cb
Kelp <kelp@androvideo.com>
2020-03-02 11:56:55 +0800

    feat: add fields `isEnable` and `port` at https certificate schema

--- https-certificate-schema.js
+++ https-certificate-schema.js
@@ -2,6 +2,40 @@ const CertificateType = require('./constants/certificate-type');
 const {validator} = require('.');
 
 module.exports = {
+  isEnable: {
+    type: 'boolean'
+  },
+  port: {
+    optional: false,
+    type: 'custom',
+    pattern: /^[\d]{1,5}$/,
+    min: 1,
+    max: 65535,
+    check: function (value, schema) {
+      if (schema.optional && (value == null || value === '')) {
+        return true;
+      }
+
+      if (typeof value !== 'string') {
+        return this.makeError('string', null, value);
+      }
+
+      if (!schema.pattern.test(value)) {
+        return this.makeError('stringPattern', schema.pattern, value);
+      }
+
+      const number = Number(value);
+      if (number < schema.min) {
+        return this.makeError('numberMin', schema.min, value);
+      }
+
+      if (number > schema.max) {
+        return this.makeError('numberMax', schema.max, value);
+      }
+
+      return true;
+    }
+  },
   certificateType: {
     optional: false,
     type: 'string',
