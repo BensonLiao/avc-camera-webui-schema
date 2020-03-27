bfd382682cc614f858383db23f7b0b87a27f4297
BensonLiao <Benson.Liao@androvideo.com>
2020-03-27 14:04:24 +0800

    Add notify type

--- constants/notification-card-type.js
+++ constants/notification-card-type.js
@@ -2,7 +2,9 @@ module.exports = {
   faceRecognition: '0',
   ageGender: '1',
   humanoidDetection: '2',
+  motionDetection: '3', // PD or MD
+  tamperAlarm: '4',
   all() {
-    return [this.faceRecognition, this.ageGender, this.humanoidDetection];
+    return [this.faceRecognition, this.ageGender, this.humanoidDetection, this.motionDetection, this.tamperAlarm];
   }
 };
