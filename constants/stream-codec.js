d4faef7d50a8fd2531747dd4cca49359e1348256
Benson <benson.liao@androvideo.com>
2020-05-26 08:22:05 +0000

    Merge branch 'feat/add-stream-codec' into 'avn'
    Feat/add stream codec off
    
    See merge request cloud/webserver-form-schema!33
    
    (cherry picked from commit a0a5f15ea496a463decbba64e2386a09966007fe)
    
    c776e133 Add stream codec 'OFF'

--- constants/stream-codec.js
+++ constants/stream-codec.js
@@ -2,7 +2,8 @@ module.exports = {
   h265: 'H265',
   h264: 'H264',
   mjpeg: 'MJPEG',
+  off: 'OFF',
   all() {
-    return [this.h265, this.h264, this.mjpeg];
+    return [this.h265, this.h264, this.mjpeg, this.off];
   }
 };
