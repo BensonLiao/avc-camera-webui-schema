92f1d785c759eca2d2c9f14b146e1deadb2570ad
rodger.lo <rodger.lo@androvideo.com>
2020-07-24 07:31:13 +0000

    Merge branch 'revert/user-permission' into 'avn'
    Revert "feat(user-permission): add viewer"
    
    See merge request cloud/webserver-form-schema!54
    
    (cherry picked from commit a9f694d7d17154d0f30c94801d8e6d888b5ad934)
    
    53138718 Revert "feat(user-permission): add viewer"

--- constants/user-permission.js
+++ constants/user-permission.js
@@ -1,9 +1,8 @@
 module.exports = {
   root: '0',
   guest: '1',
-  viewer: '2',
   superAdmin: '99',
   all() {
-    return [this.root, this.guest, this.viewer, this.superAdmin];
+    return [this.root, this.guest, this.superAdmin];
   }
 };
