a5f3167f81572db3d94554c51f5ad1e47dd87a0b
BensonLiao <Benson.Liao@androvideo.com>
2020-10-23 13:35:39 +0800

    refactor(i18n): change data structure to object

--- constants/i18n-supported-code.js
+++ constants/i18n-supported-code.js
@@ -1,7 +1,17 @@
-module.exports = [
-  {code: 'en', title: 'English'},
-  {code: 'zh-tw', title: '中文(繁體)'},
-  {code: 'zh-cn', title: '中文(简体)'},
-  {code: 'ja', title: '日本語'},
-  {code: 'es', title: 'Español'}
-];
+module.exports = {
+  en: {
+    title: 'English'
+  },
+  'zh-tw': {
+    title: '中文(繁體)'
+  },
+  'zh-cn': {
+    title: '中文(简体)'
+  },
+  ja: {
+    title: '日本語'
+  },
+  es: {
+    title: 'Español'
+  }
+};
