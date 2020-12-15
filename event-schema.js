86d4a7d94a61e055ae5d797c263822f20bf9e764
kevin.kao <Kevin.Kao@androvideo.com>
2020-12-15 14:47:53 +0800

    add event search form schema

--- event-schema.js
+++ event-schema.js
@@ -8,5 +8,23 @@ module.exports = {
     optional: false,
     type: 'date',
     convert: true
+  },
+  keyword: {
+    optional: true,
+    type: 'custom',
+    check: function (value) {
+      const plus = /\+/g;
+      const space = /\s/g;
+
+      if (((value.match(plus) || []).length > 3) || (value.match(space) || []).length > 3) {
+        return this.makeError('maxOperators', null, value);
+      }
+
+      if (value.match(plus) && value.match(space)) {
+        return this.makeError('mixOperators', null, value);
+      }
+
+      return true;
+    }
   }
 };
